e5b9901fcf12dc96bb109625c87bca52b8a32ca2
Maksym Solomkin <maksym@solomk.in>
2022-12-17 20:07:33 +0200

    Configure to use axion release plugin

--- dictionary-server/build.gradle.kts
+++ dictionary-server/build.gradle.kts
@@ -4,10 +4,11 @@ plugins {
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
     id("org.graalvm.buildtools.native") version "0.9.18"
+    id("pl.allegro.tech.build.axion-release") version "1.14.0"
 }
 
 group = "in.solomk"
-version = "0.0.1-SNAPSHOT"
+project.version = scmVersion.version
 java.sourceCompatibility = JavaVersion.VERSION_19
 
 configurations {
@@ -18,6 +19,12 @@ configurations {
 
 repositories {
     mavenCentral()
+}
+
+scmVersion {
+    repository {
+        directory.set(project.rootProject.file("../"))
+    }
 }
 
 dependencies {
