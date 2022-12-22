b4793f5e1eab69bd1281c94536b170a393799d8b
Maksym Solomkin <maksym@solomk.in>
2022-12-22 17:33:28 +0200

    Base OAuth2 login sample

--- dictionary-server/build.gradle.kts
+++ dictionary-server/build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     compileOnly("org.projectlombok:lombok")
