bd851573ff2c9c7d536614690ea8813db0ae1ce7
Maksym Solomkin <maksym@solomk.in>
2023-01-03 20:39:06 +0200

    Add basic profile page

--- dictionary-client/src/client/dictionary-client.ts
+++ dictionary-client/src/client/dictionary-client.ts
@@ -2,6 +2,8 @@ import {UnsavedWord} from "./model/unsaved-word"
 import {UserWords} from "./model/user-words"
 import {Word} from "./model/word";
 import {RestClient} from "./rest-client";
+import jwtDecode, {JwtPayload} from "jwt-decode";
+import {UserDetails} from "./model/user-details";
 
 export function dictionaryClient() {
     return new DictionaryClient(new RestClient())
@@ -31,6 +33,16 @@ class DictionaryClient {
         return userWords
     }
 
+    getUserDetails(): UserDetails {
+        const token = this.extractJwtToken();
+        const decodedToken = jwtDecode<JwtPayload>(token)
+        const userId = decodedToken.sub
+        if (!userId) {
+            throw new Error('No user id found in token')
+        }
+        return {userId: userId}
+    }
+
     private extractJwtToken(): string {
         const token = localStorage.getItem('token')
         if (!token) {
@@ -38,5 +50,4 @@ class DictionaryClient {
         }
         return token
     }
-
 }
