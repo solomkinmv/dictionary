b7350e01d79788bd76f219d07c4725b83d35edc6
Maksym Solomkin <maksym@solomk.in>
2023-01-05 22:32:46 +0200

    Add support for expired access tokens

--- dictionary-client/src/components/auth/authentication-helpers.tsx
+++ dictionary-client/src/components/auth/authentication-helpers.tsx
@@ -20,11 +20,14 @@ let AuthContext = React.createContext<AuthContextType>(null!);
 export function AuthProvider({children}: { children: React.ReactNode }) {
     let [token, setToken] = React.useState<string>(localStorage.getItem("token") || "");
     let [user, setUser] = React.useState<string>(token ? extractUserIdClaimFromToken(token) : "");
+    let [expiresAt, setExpiresAt] = React.useState<number>(token ? extractExpirationClaimFromToken(token) : 0);
 
     let signin = (token: string, callback: VoidFunction) => {
         const userId = extractUserIdClaimFromToken(token);
+        const expiresAt = extractExpirationClaimFromToken(token);
         localStorage.setItem("token", token);
         setUser(userId);
+        setExpiresAt(expiresAt);
         setToken(token);
         callback();
     };
@@ -37,7 +40,7 @@ export function AuthProvider({children}: { children: React.ReactNode }) {
     };
 
     let isAuthenticated = () => {
-        return !!user;
+        return !!user && new Date().getTime() < expiresAt;
     }
 
     let value = {user, token, signin, signout, isAuthenticated};
@@ -52,6 +55,15 @@ function extractUserIdClaimFromToken(token: string): string {
         throw new Error('No user id found in token')
     }
     return userId
+}
+
+function extractExpirationClaimFromToken(token: string): number {
+    const decodedToken = jwtDecode<JwtPayload>(token)
+    const expiration = decodedToken.exp
+    if (!expiration) {
+        throw new Error('No expiration found in token')
+    }
+    return expiration
 }
 
 export default function useAuth() {
