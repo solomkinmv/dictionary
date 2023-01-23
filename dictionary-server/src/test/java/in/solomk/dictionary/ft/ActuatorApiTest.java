02f1fd5ad023af3bc6d75e149e3fa3a6be3c00dc
Maksym Solomkin <maksym@solomk.in>
2023-01-23 20:10:06 +0200

    Expose all actuator endpoints for authorized users

--- dictionary-server/src/test/java/in/solomk/dictionary/ft/ActuatorApiTest.java
+++ dictionary-server/src/test/java/in/solomk/dictionary/ft/ActuatorApiTest.java
@@ -1,12 +1,16 @@
 package in.solomk.dictionary.ft;
 
+import in.solomk.dictionary.ft.client.ActuatorTestClient;
 import org.junit.jupiter.api.Test;
+
+import static in.solomk.dictionary.ft.client.ActuatorTestClient.AuthenticationMode.AUTHENTICATED;
+import static in.solomk.dictionary.ft.client.ActuatorTestClient.AuthenticationMode.INVALID_CREDENTIALS;
 
 public class ActuatorApiTest extends BaseFuncTest {
 
     @Test
     void returnsBasicHealthWhenNotAuthenticated() {
-        actuatorTestClient.getHealth(false)
+        actuatorTestClient.getHealth(ActuatorTestClient.AuthenticationMode.UNAUTHENTICATED)
                           .expectStatus()
                           .isOk()
                           .expectBody()
@@ -18,7 +22,7 @@ public class ActuatorApiTest extends BaseFuncTest {
 
     @Test
     void returnsDetailedHealthWhenAuthenticated() {
-        actuatorTestClient.getHealth(true)
+        actuatorTestClient.getHealth(AUTHENTICATED)
                           .expectStatus()
                           .isOk()
                           .expectBody()
@@ -47,12 +51,119 @@ public class ActuatorApiTest extends BaseFuncTest {
 
     @Test
     void returnsInfo() {
-        actuatorTestClient.getInfo(true)
+        actuatorTestClient.getInfo(AUTHENTICATED)
                           .expectStatus()
                           .isOk()
                           .expectBody()
                           .json("""
                                         {}
                                         """, true);
+    }
+
+    @Test
+    void returnsUnauthorizedErrorForEnvEndpointIfNotAuthenticated() {
+        actuatorTestClient.getEnv(ActuatorTestClient.AuthenticationMode.UNAUTHENTICATED)
+                          .expectStatus()
+                          .isUnauthorized();
+    }
+
+    @Test
+    void returnsUnauthorizedErrorForMetricsIfCredentialsInvalid() {
+        actuatorTestClient.getMetrics(INVALID_CREDENTIALS)
+                          .expectStatus()
+                          .isUnauthorized();
+    }
+
+    @Test
+    void returnsListOfActuatorEndpoints() {
+        actuatorTestClient.getAllActuatorEndpoints(AUTHENTICATED)
+                          .expectStatus()
+                          .isOk()
+                          .expectBody()
+                          .json("""
+                                        {
+                                          "_links": {
+                                            "self": {
+                                              "href": "/actuator",
+                                              "templated": false
+                                            },
+                                            "beans": {
+                                              "href": "/actuator/beans",
+                                              "templated": false
+                                            },
+                                            "caches-cache": {
+                                              "href": "/actuator/caches/{cache}",
+                                              "templated": true
+                                            },
+                                            "caches": {
+                                              "href": "/actuator/caches",
+                                              "templated": false
+                                            },
+                                            "health-path": {
+                                              "href": "/actuator/health/{*path}",
+                                              "templated": true
+                                            },
+                                            "health": {
+                                              "href": "/actuator/health",
+                                              "templated": false
+                                            },
+                                            "info": {
+                                              "href": "/actuator/info",
+                                              "templated": false
+                                            },
+                                            "conditions": {
+                                              "href": "/actuator/conditions",
+                                              "templated": false
+                                            },
+                                            "configprops": {
+                                              "href": "/actuator/configprops",
+                                              "templated": false
+                                            },
+                                            "configprops-prefix": {
+                                              "href": "/actuator/configprops/{prefix}",
+                                              "templated": true
+                                            },
+                                            "env": {
+                                              "href": "/actuator/env",
+                                              "templated": false
+                                            },
+                                            "env-toMatch": {
+                                              "href": "/actuator/env/{toMatch}",
+                                              "templated": true
+                                            },
+                                            "loggers": {
+                                              "href": "/actuator/loggers",
+                                              "templated": false
+                                            },
+                                            "loggers-name": {
+                                              "href": "/actuator/loggers/{name}",
+                                              "templated": true
+                                            },
+                                            "heapdump": {
+                                              "href": "/actuator/heapdump",
+                                              "templated": false
+                                            },
+                                            "threaddump": {
+                                              "href": "/actuator/threaddump",
+                                              "templated": false
+                                            },
+                                            "metrics": {
+                                              "href": "/actuator/metrics",
+                                              "templated": false
+                                            },
+                                            "metrics-requiredMetricName": {
+                                              "href": "/actuator/metrics/{requiredMetricName}",
+                                              "templated": true
+                                            },
+                                            "scheduledtasks": {
+                                              "href": "/actuator/scheduledtasks",
+                                              "templated": false
+                                            },
+                                            "mappings": {
+                                              "href": "/actuator/mappings",
+                                              "templated": false
+                                            }
+                                          }
+                                        }""", true);
     }
 }
--- dictionary-server/src/test/java/in/solomk/dictionary/ft/client/ActuatorTestClient.java
+++ dictionary-server/src/test/java/in/solomk/dictionary/ft/client/ActuatorTestClient.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
 import java.util.function.Consumer;
 
@@ -13,26 +14,56 @@ public class ActuatorTestClient {
 
     private final WebTestClient webTestClient;
 
-    public WebTestClient.ResponseSpec getHealth(boolean authenticated) {
+    public ResponseSpec getHealth(AuthenticationMode authenticationMode) {
         return webTestClient.get()
                             .uri("/actuator/health")
-                            .headers(addAuthHeader(authenticated))
+                            .headers(addAuthHeader(authenticationMode))
                             .exchange();
     }
 
-    public WebTestClient.ResponseSpec getInfo(boolean authenticated) {
+    public ResponseSpec getInfo(AuthenticationMode authenticationMode) {
         return webTestClient.get()
                             .uri("/actuator/info")
-                            .headers(addAuthHeader(authenticated))
+                            .headers(addAuthHeader(authenticationMode))
+                            .exchange();
+    }
+
+    public ResponseSpec getAllActuatorEndpoints(AuthenticationMode authenticationMode) {
+        return webTestClient.get()
+                            .uri("/actuator")
+                            .headers(addAuthHeader(authenticationMode))
+                            .exchange();
+    }
+
+    public ResponseSpec getEnv(AuthenticationMode authenticationMode) {
+        return webTestClient.get()
+                            .uri("/actuator/env")
+                            .headers(addAuthHeader(authenticationMode))
+                            .exchange();
+    }
+
+    public ResponseSpec getMetrics(AuthenticationMode authenticationMode) {
+        return webTestClient.get()
+                            .uri("/actuator/metrics")
+                            .headers(addAuthHeader(authenticationMode))
                             .exchange();
     }
 
 
-    private Consumer<HttpHeaders> addAuthHeader(boolean authenticated) {
+    private Consumer<HttpHeaders> addAuthHeader(AuthenticationMode authenticationMode) {
         return headers -> {
-            if (authenticated) {
+            if (authenticationMode == AuthenticationMode.AUTHENTICATED) {
                 headers.setBasicAuth("admin", "secret");
             }
+            if (authenticationMode == AuthenticationMode.INVALID_CREDENTIALS) {
+                headers.setBasicAuth("admin", "invalid");
+            }
         };
+    }
+
+    public enum AuthenticationMode {
+        AUTHENTICATED,
+        UNAUTHENTICATED,
+        INVALID_CREDENTIALS
     }
 }
